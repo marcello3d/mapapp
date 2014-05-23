f576156dfffe63ebe2e6b5d7e27808eedc475562
Marcello Bastea-Forte <marcello@cellosoft.com>
2014-05-23 12:43:52 -0700

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,178 +1,204 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
 
-var clean = require('gulp-clean');
-var livereload = require('gulp-livereload');
+var clean = require('gulp-clean')
+var livereload = require('gulp-livereload')
 
-var jshint = require('gulp-jshint');
-var watchify = require('./gulp-watchify');
-var uglify = require('./gulp-uglify');
-var imagemin = require('gulp-imagemin');
-var autoprefixer = require('gulp-autoprefixer');
-var csso = require('gulp-csso');
-var htmlmin = require('gulp-htmlmin');
+var jshint = require('gulp-jshint')
+var watchify = require('gulp-watchify')
+var uglify = require('gulp-uglify')
+var streamify = require('gulp-streamify')
+var imagemin = require('gulp-imagemin')
+var autoprefixer = require('gulp-autoprefixer')
+var csso = require('gulp-csso')
+var svgmin = require('gulp-svgmin')
+var htmlmin = require('gulp-htmlmin')
+var bust = require('gulp-buster')
 
-var gutil = require('gulp-util');
+var HTTP_SERVER_PORT = 9000
 
-var HTTP_SERVER_PORT = 9000;
+/////////////////////////////////////////////////////////////////////////////
+// Paths you might care about
 
-var paths = {
-    js: [
-        // All JavaScript files (for syntax validation and linting)
-        '*.js',
-        'server/**/*.js',
-        'client/js/**/*.js'
-    ],
-    client: {
-        build: 'build/',
-        html: {
-            src:[
-                'client/**/*.html'
-            ],
-            dest: 'build/'
-        },
-        browserify: {
-            src: [
-                'client/js/**/*.js',
-                "!client/js/**/lib/**" // Don't bundle libs
-            ],
-            dest:'build/js/'
-        },
-        css: {
-            src:[
-                'client/css/**/*.css',
-                "!client/css/**/lib/**" // Don't bundle libs
-            ],
-            dest:'build/css/'
-        },
-        images: {
-            src:[
-                'client/img/**/*'
-            ],
-            dest:'build/img/'
-        }
-    }
-};
+var ALL_JS = [
+    // All JavaScript files (for syntax validation and linting)
+    '*.js',
+    'server/**/*.js',
+    'client/js/**/*.js'
+]
+var CLIENT_BUILD = 'build/'
 
+var HTML_SRC = [ 'client/**/*.html' ]
+var HTML_DEST = CLIENT_BUILD
+
+var BROWSERIFY_SRC = [
+    'client/js/**/*.js',
+    "!client/js/**/lib/**" // Don't bundle libs
+]
+var BROWSERIFY_DEST = CLIENT_BUILD + 'js/'
+
+var CSS_SRC = [
+    'client/css/**/*.css',
+    "!client/css/**/lib/**" // Don't bundle libs
+]
+var CSS_DEST = CLIENT_BUILD + 'css/'
+
+var IMAGEMIN_SRC = [
+    'client/img/**/*.png',
+    'client/img/**/*.gif',
+    'client/img/**/*.jpg',
+    'client/img/**/*.jpeg'
+]
+var SVGMIN_SRC = [
+    'client/img/**/*.svg'
+]
+var IMAGE_DEST = CLIENT_BUILD + 'img/'
+
+var STATIC_MANIFEST_FILE = 'manifest.json'
+var STATIC_MANIFEST_DEST = CLIENT_BUILD + STATIC_MANIFEST_FILE
+var STATIC_MANIFEST_SRC = [
+    CLIENT_BUILD + '**/*',
+    '!' + STATIC_MANIFEST_DEST
+]
+
+
+/////////////////////////////////////////////////////////////////////////////
 
 
 // Hack to enable configurable watchify watching
-var watching = false;
-gulp.task('enable-watch-mode', function() { watching = true; });
+var watching = false
+gulp.task('enable-watch-mode', function() { watching = true })
 
 function markAsFailed(err) {
-    console.error(err.stack);
+    console.error(err.stack)
     if (watching) {
-        gutil.log("Continuing despite error... (watch mode)")
+        console.log("Continuing despite error... (watch mode)")
     } else {
-        process.exit(1);
+        process.exit(1)
     }
 }
 
 // Browserify and copy js files
 gulp.task('browserify', watchify(function scriptsTask(watchify) {
-    return gulp.src(paths.client.browserify.src)
+    return gulp.src(BROWSERIFY_SRC)
         .pipe(watchify({
             watch:watching,
-            configure:function(b) {
-                b.transform('ractify');
+            setup: function(bundle) {
+                bundle.transform(require('ractify'))
             }
         }))
-//        .pipe(uglify())
-        .pipe(gulp.dest(paths.client.browserify.dest))
-        .on('error', markAsFailed);
-}));
+        .pipe(streamify(uglify()))
+        .pipe(gulp.dest(BROWSERIFY_DEST))
+        .on('error', markAsFailed)
+}))
 
-gulp.task('watchify', ['enable-watch-mode', 'browserify']);
+gulp.task('watchify', ['enable-watch-mode', 'browserify'])
 
 // Validate JavaScript
 gulp.task('lint-js', function(){
-    return gulp.src(paths.js)
+    return gulp.src(ALL_JS)
         .pipe(jshint())
         .pipe(jshint.reporter(require('jshint-stylish')))
         .pipe(jshint.reporter('fail'))
-        .on('error', markAsFailed);
-});
+        .on('error', markAsFailed)
+})
 
 
 // Minify and copy all css
 gulp.task('css', function() {
-    return gulp.src(paths.client.css.src)
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .on('error', markAsFailed)
+    return gulp.src(CSS_SRC)
+        .pipe(autoprefixer('last 5 version', 'safari 4', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(csso())
-        .pipe(gulp.dest(paths.client.css.dest))
-        .on('error', markAsFailed);
-});
+        .pipe(gulp.dest(CSS_DEST))
+        .on('error', markAsFailed)
+})
 
-// Optimize and copy static images
-gulp.task('images', function() {
-    return gulp.src(paths.client.images.src)
+// Optimize png/gif/jpg/jpeg
+gulp.task('imagemin', function() {
+    return gulp.src(IMAGEMIN_SRC)
         // Pass in options to the task
         .pipe(imagemin({optimizationLevel: 5}))
-        .pipe(gulp.dest(paths.client.images.dest))
-        .on('error', markAsFailed);
-});
+        .pipe(gulp.dest(IMAGE_DEST))
+        .on('error', markAsFailed)
+})
+
+// Optimize svgs
+gulp.task('svgmin', function() {
+    return gulp.src(SVGMIN_SRC)
+        // Pass in options to the task
+        .pipe(svgmin())
+        .pipe(gulp.dest(IMAGE_DEST))
+        .on('error', markAsFailed)
+})
 
 
 // Minify and copy html files
 gulp.task('html', function() {
-    return gulp.src(paths.client.html.src)
+    return gulp.src(HTML_SRC)
         .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest(paths.client.html.dest))
-        .on('error', markAsFailed);
-});
+        .pipe(gulp.dest(HTML_DEST))
+        .on('error', markAsFailed)
+})
+
+// Generate hashes
+gulp.task('hash-generation', function() {
+    return gulp.src(STATIC_MANIFEST_SRC)
+        .pipe(bust(STATIC_MANIFEST_FILE))
+        .pipe(gulp.dest(CLIENT_BUILD))
+})
 
 // Run static HTTP server
 gulp.task('static-server', function(next) {
-    var http = require('http');
-    var NodeStatic = require('node-static');
+    var http = require('http')
+    var NodeStatic = require('node-static')
 
-    var server = new NodeStatic.Server(paths.client.html.dest);
+    var server = new NodeStatic.Server(HTML_DEST)
     var httpServer = http.createServer(function (request, response) {
         request.addListener('end', function () {
-            server.serve(request, response);
-        }).resume();
+            server.serve(request, response)
+        }).resume()
     }).listen(HTTP_SERVER_PORT, function() {
-        gutil.log('Static file server listening: ' + gutil.colors.magenta('http://'+httpServer.address().address+":"+httpServer.address().port));
-        next();
-    });
-});
+            console.log('\nDEVELOPMENT-ONLY file server listening: http://'+httpServer.address().address+":"+httpServer.address().port+'\n')
+            next()
+        })
+})
 
 
 // Rerun tasks when a file changes
-gulp.task('watch', ['watchify', 'css', 'images', 'html', 'validate'], function () {
-    gulp.watch(paths.client.css.src, ['css']);
-    gulp.watch(paths.client.images.src, ['images']);
-    gulp.watch(paths.client.html.src, ['html']);
-    gulp.watch(paths.js, ['lint-js']);
+gulp.task('watch', ['enable-watch-mode', 'build', 'validate'], function () {
+    gulp.watch(CSS_SRC, ['css'])
+    gulp.watch(IMAGEMIN_SRC, ['imagemin'])
+    gulp.watch(SVGMIN_SRC, ['svgmin'])
+    gulp.watch(HTML_SRC, ['html'])
+    gulp.watch(ALL_JS, ['lint-js'])
+    gulp.watch(STATIC_MANIFEST_SRC, ['hash-generation'])
     gulp.watch('gulpfile.js', function() {
-        console.error("\nWarning! gulpfile.js has changed, but you'll need to restart gulp to see them\n");
-    });
-});
+        console.error("\nWarning! gulpfile.js has changed, but you'll need to restart gulp to see them\n")
+    })
+})
 
-gulp.task('serve', ['enable-watch-mode', 'watch', 'static-server', 'livereload-server']);
+gulp.task('serve', ['watch', 'static-server', 'livereload-server'])
 
 
 // Live reload server detects any changes in our build directory
 gulp.task('livereload-server', function () {
-    var server = livereload();
-    gulp.watch(paths.client.build+'/**/*').on('change', function(file) {
-        server.changed(file.path);
-    });
-});
+    var server = livereload()
+    gulp.watch(CLIENT_BUILD+'/**/*').on('change', function(file) {
+        server.changed(file.path)
+    })
+})
 
 // clean out built files
 gulp.task('clean', function() {
-    return gulp.src(paths.client.build, {read: false})
-               .pipe(clean())
-               .on('error', markAsFailed);
-});
+    return gulp.src(CLIENT_BUILD, {read: false})
+        .pipe(clean())
+        .on('error', markAsFailed)
+})
 
 // Build only
-gulp.task('build', ['browserify', 'css', 'html', 'images']);
+gulp.task('build', ['browserify', 'css', 'html', 'imagemin', 'svgmin', 'hash-generation'])
 
 // Build and validate
-gulp.task('validate', ['lint-js']);
+gulp.task('validate', ['lint-js'])
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['validate', 'build']);
+gulp.task('default', ['validate', 'build'])
